95fb1b20bbc152b925a4794ac13bf01c9c69dbb6
Felipe <felipeschmitz123@gmail.com>
2017-10-16 23:07:02 -0200

    refatorando

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
 });             
 
 function filtarLista() {
-    // Declare variables
+    // declarando as variaveis
     var input, filter, ul, li, a, i;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
@@ -68,7 +68,7 @@ function filtarLista() {
     li = ul.getElementsByTagName('li');
     
 
-    // Loop through all list items, and hide those who don't match the search query
+    // faz o loop pelos itens da lista e esconde os que não batem com a pesquisa
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
